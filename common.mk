87804e2a12061e1221f5bead274c2802f5752be2
John Galt <johngaltfirstrun@gmail.com>
2023-02-27 08:46:57 -0500

    inherit Adreno + drop

--- common.mk
+++ common.mk
@@ -12,6 +12,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
+# Get Adreno aspects
+$(call inherit-product, device/qcom/common/vendor/adreno-6xx-legacy/qti-adreno-6xx-legacy.mk)
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
