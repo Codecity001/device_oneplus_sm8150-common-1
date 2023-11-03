b55819ddfe0aaf7086008da3235982413d2c221d
Master <mhmdeveloperofficial@gmail.com>
2023-11-03 04:49:21 +0000

    sm8150-common: ViPER4Android FX support *Prerequisite: git clone https://github.com/swiitch-OFF-Lab/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
    Signed-off-by: Harish <sanganiharish263@gmail.com>

--- common.mk
+++ common.mk
@@ -15,6 +15,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Derp versioning
 DERP_VERSION_APPEND_TIME_OF_DAY := true
 
+# Viper
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
