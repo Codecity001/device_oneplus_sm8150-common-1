262e2fa512c8eacf2acff4093e6a726a2abfd906
Codecity001 <sanganiharish263@gmail.com>
2025-07-30 14:30:02 -0500

    [TEMP]sm8150-common: Set Insecure_Adb to true
    Signed-off-by: Codecity001 <sanganiharish263@gmail.com>

--- common.mk
+++ common.mk
@@ -72,6 +72,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Adb
+WITH_ADB_INSECURE := true
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     liba2dpoffload
